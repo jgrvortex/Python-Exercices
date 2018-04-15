c373918fc6a5607bf279276254e66461488a4a78
jgrvortex <jgr.vortex@gmail.com>
2018-04-15 02:29:56 +0200

    Añadiendo otro archivo

---
+++ mostrarnumero10.py
@@ -0,0 +1,8 @@
+numero=10
+
+while numero>0:
+    print(numero)
+    numero-=1
+
+print("El numero 10 no aparecía porque el decremento de la variable 'numero' se ejecutaba antes de su")
+print("impresion dentro del bloque while, por lo que en la primera iteracion del ciclo el valor impreso era 9")
