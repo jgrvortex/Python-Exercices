2d845c6cfbbd8452f2603fc4ded76e00afbde87b
jgrvortex <jgr.vortex@gmail.com>
2018-04-15 02:31:12 +0200

    Añadiendo un programilla de práctica libre

---
+++ opciones.py
@@ -0,0 +1,23 @@
+numero=int(input("Elige un numero: "))
+
+if (numero<100 and numero>10) and numero%2==0:
+    print("El numero introducido es par y esta dentro del intervalo permitido")
+elif (numero<100 and numero>10) and numero%2!=0:
+    print("El numero introducido es impar y esta dentro del intervalo permitodo")
+else:
+    print("El numero esta fuera del intervalo permitido")
+
+while numero>0:
+    print("El numero actual es: {}".format(numero))
+    divs=int(numero/2+1)
+    while divs>1:
+        noprimo=(numero%divs==0)
+        if noprimo==True:
+            break
+        divs -= 1
+
+    if noprimo==True:
+        print("Se trata de un numero no primo")
+    else:
+        print("Se trata de un numero primo")
+    numero -= 1
