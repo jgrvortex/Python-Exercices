27185eb3f731751fea4166c0a105fc765eb2ae5a
jgrvortex <jgr.vortex@gmail.com>
2018-04-16 19:16:35 +0200

    Añadiendo el ejercicio de calculadora

---
+++ calculadora_ejercicio.py
@@ -0,0 +1,28 @@
+operacion = input("¿Que operacion deseas realizar? (multiplicar, dividir, sumar, restar): ")
+
+primer_numero = int(input("Primer numero: "))
+segundo_numero = int(input("Segundo numero: "))
+
+
+opcion_correcta = False
+
+while opcion_correcta == False:
+
+    if operacion == "multiplicar":
+        print("El resultado es: {}".format(primer_numero*segundo_numero))
+        opcion_correcta = True
+    elif operacion == "dividir":
+        resul = float(primer_numero / segundo_numero)
+        print("El resultado es: {}".format(resul))
+        opcion_correcta = True
+    elif operacion == "sumar":
+        print("El resultado es: {}".format(primer_numero+segundo_numero))
+        opcion_correcta = True
+    elif operacion == "restar":
+        if primer_numero < segundo_numero:
+            print("El resultado es: {}".format(segundo_numero-primer_numero))
+        else:
+            print("El resultado es: {}".format(primer_numero-segundo_numero))
+        opcion_correcta = True
+    else:
+        print("Debe de elegir una operacion valida: ")
