ebd02f24690de4fdc6b686c7598172adb83377a6
jgrvortex <jgr.vortex@gmail.com>
2018-04-19 19:52:43 +0200

    Merge branch 'master' of C:\Users\Javi\PycharmProjects\programaprueba1 with conflicts.

---
+++ mayor.py
@@ -0,0 +1,8 @@
+
+primer=int(input("Primer numero: "))
+segundo=int(input("Segundo numero: "))
+tercer=int(input("Tercer numero: "))
+
+resultado=max(primer,segundo,tercer)
+
+print("El numero mas grande es {}  {}".format(resultado,resultado+1))
---
+++ test_imput.py
@@ -0,0 +1,6 @@
+
+numero_del_usuario=input("Introduce un numero: ")
+if(numero_del_usuario)>"10":
+    print("El número es mayor que 10")
+else:
+    print("El número es menor que 10")
